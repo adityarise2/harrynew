feb9a79b614afbfedaeaeaa6e94ec4272baed0a6
Aditya <137521434+adityarise2@users.noreply.github.com>
2024-08-11 11:43:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1001914728318'))
 request_channel = environ.get('REQUEST_CHANNEL', '-1001951553426')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1001899266690'))
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1001916993962'))
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002115008104'))
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
 PORT = os.environ.get('PORT', '5000')
